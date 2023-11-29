ecfb8063b6253cea104283fb6a2502572435f8cc
AllaizaCerio <allaizacerio09@gmail.com>
2023-11-29 23:01:53 +0800

    Delete src directory

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers:{
-       Authorization: 'Bearer DYo9x7dbqxoXfQFT1e71ryP_6G-raLJ0e1o-8p5OYGcSOcTIWxT1GqTwr6WakvbSbIZGtRdwlQSMnncvi4uoWo4z_FXblgY12RGZ0_Qp3Gj0bZEf-9mXblqUri8cZXYx' 
-    }
-});
